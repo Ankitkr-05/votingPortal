d3f4c3c9ada0207a93931c10635f92504eab4cae
AnkitKr-05 <kr.ankit603@gmail.com>
2021-10-10 19:49:06 +0530

    Updates

---
+++ precache-manifest.17c70d806584f6b4adb4e76deb2fcecb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5242682199cdc056dbd43bd63a7a1559",
+    "url": "/votingPortal/index.html"
+  },
+  {
+    "revision": "4d7f46cfcd6cbeaeb544",
+    "url": "/votingPortal/static/css/main.d9bedae0.chunk.css"
+  },
+  {
+    "revision": "6f3f8b83cfcee6c4c6d6",
+    "url": "/votingPortal/static/js/2.ac236db1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/votingPortal/static/js/2.ac236db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7f46cfcd6cbeaeb544",
+    "url": "/votingPortal/static/js/main.c4e87736.chunk.js"
+  },
+  {
+    "revision": "74e36d86f42dbc6dc45e",
+    "url": "/votingPortal/static/js/runtime-main.006739e2.js"
+  },
+  {
+    "revision": "71d778ee4534743520764452e62ee3dd",
+    "url": "/votingPortal/static/media/amit_shah.71d778ee.jpg"
+  },
+  {
+    "revision": "0978e1e9fbadcbbdca69651e5a651a24",
+    "url": "/votingPortal/static/media/bjp.0978e1e9.png"
+  },
+  {
+    "revision": "52ec6c4c64e1bfcf1de948d097eae759",
+    "url": "/votingPortal/static/media/congress.52ec6c4c.png"
+  },
+  {
+    "revision": "b6650b6880a0d347a3db3c33e0fdec14",
+    "url": "/votingPortal/static/media/manmohan_singh.b6650b68.jpg"
+  },
+  {
+    "revision": "a588530e10bf26e35b11d085c28312c2",
+    "url": "/votingPortal/static/media/modi.a588530e.jpg"
+  },
+  {
+    "revision": "b597fc68e67b926b63b9ccbded1604de",
+    "url": "/votingPortal/static/media/rahul-gandhi.b597fc68.jpg"
+  }
+]);
